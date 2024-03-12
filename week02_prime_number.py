7009ed7636a3c7637f107b5e6655f5de06041f04
dldydqls <yblee0328@gmail.com>
2024-03-12 20:57:53 +0900

    v0.4 week02_prime_number

--- week02_prime_number.py
+++ week02_prime_number.py
@@ -1,16 +1,19 @@
 number = int(input("Input number : "))
 
+is_prime_number = True
 if number < 2:
-    count = 1
+    is_prime_number = False
 else:
-    count = 0
+    is_prime_number = 0
     for i in range (2, number):
         if number % i == 0:
-            count = count + 1
-            break
+            #is_primenumber = is_prime_number + 1
+            is_prime_number = False  #Remove the plus operation
+            break  #Exit the loop when the first divisor is found.
         print(1, end=" ")
 
-if count == 0:
+#if is_prime_number == 0:
+if is_prime_number:
     print(f"{number} is prime number")
 else:
     print(f"{number} is NOT prime number")
