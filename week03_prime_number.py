02a49a1991f41275ebbc1d5f66dc6e916af1713f
dldydqls <yblee0328@gmail.com>
2024-03-19 19:22:14 +0900

    v1.1 week03_prime_number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,17 +1,16 @@
-number = int(input("Input number : "))
+start = int(input("Input start number : "))
+end = int(input("Input end number : "))
 
-is_prime_number = True
-if number < 2:
-    is_prime_number = False
-else:
-    i = 2
-    while i*i <= number:
-        if number % i == 0:
-            is_prime_number = False
-            break
-        i += 1
-
-if is_prime_number:
-    print(f"{number} is prime number")
-else:
-    print(f"{number} is NOT prime number")
+for k in range (start, end+1):
+    is_prime_number = True
+    if k < 2:
+        is_prime_number = False
+    else:
+        i = 2
+        while i*i <= k:
+            if k % i == 0:
+                is_prime_number = False
+                break
+            i += 1
+        if is_prime_number:
+            print(k, end= " ")
